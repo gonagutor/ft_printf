82c85c3603564541a0227f7958a06341f58a2fcb
Gonzalo Aguado Torres <gonagutor@gmail.com>
2021-03-01 12:29:09 +0100

    %s now fully works

--- srcs/ft_sflag.c
+++ srcs/ft_sflag.c
@@ -6,7 +6,7 @@
 /*   By: gaguado- <gaguado-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 15:33:08 by gaguado-          #+#    #+#             */
-/*   Updated: 2021/02/19 14:24:49 by gaguado-         ###   ########.fr       */
+/*   Updated: 2021/03/01 12:26:42 by gaguado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,10 @@ int	ft_sflag(t_flags flg, va_list args)
 		str = "(null)\0";
 	if (flg.dot_mod && flg.prec_mod > -1)
 	{
-		ft_print_char_repeatedly(' ', flg.flagqtt_mod - flg.prec_mod);
+		if ((int)ft_strlen(str) < flg.prec_mod)
+			ft_print_char_repeatedly(' ', flg.flagqtt_mod - ft_strlen(str));
+		else
+			ft_print_char_repeatedly(' ', flg.flagqtt_mod - flg.prec_mod);
 		ret += ft_putstr_upton(str, flg.prec_mod);
 	}
 	else
