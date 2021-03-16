16a3a59c087826a120b2dfb51130d021bd984681
Gonzalo Aguado Torres <gonagutor@gmail.com>
2021-03-16 12:57:01 +0100

    D flag now fully working

--- srcs/ft_dflag.c
+++ srcs/ft_dflag.c
@@ -6,7 +6,7 @@
 /*   By: gaguado- <gaguado-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 17:02:01 by gaguado-          #+#    #+#             */
-/*   Updated: 2021/03/10 19:31:37 by gaguado-         ###   ########.fr       */
+/*   Updated: 2021/03/16 12:56:30 by gaguado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,20 @@ int	ft_dspaces(t_flags flg, long num, int neg)
 	spaces = (int)(num != 0 || flg.dot_mod == 0);
 	if (neg)
 	{
-		if (ft_abs(flg.prec_mod) <= ft_ncsigned(num) + 1 + (num < 0) && flg.dot_mod)
-			spaces = (flg.flagqtt_mod * -1) - ft_abs(flg.prec_mod) - spaces;
+		if (ft_abs(flg.prec_mod) <= ft_ncsigned(num) + 1 + (num < 0)
+			&& flg.dot_mod)
+			spaces = (flg.flagqtt_mod * -1) - ft_abs(flg.prec_mod) - spaces
+			- (num < 0 && ft_abs(flg.prec_mod) <= ft_ncsigned(num) + 1);
 		else
 			spaces = (flg.flagqtt_mod * -1) - ft_abs(ft_abs(flg.prec_mod)
 				- ft_ncsigned(num)) - spaces;
 	}
 	else
 	{
-		if (ft_abs(flg.prec_mod) <= ft_ncsigned(num) + 1 + (num < 0) && flg.dot_mod)
-			spaces = flg.flagqtt_mod - ft_abs(flg.prec_mod) - spaces;
+		if (ft_abs(flg.prec_mod) <= ft_ncsigned(num) + 1 + (num < 0)
+			&& flg.dot_mod)
+			spaces = flg.flagqtt_mod - ft_abs(flg.prec_mod) - spaces
+			- (num < 0 && ft_abs(flg.prec_mod) <= ft_ncsigned(num) + 1);
 		else
 			spaces = flg.flagqtt_mod - ft_abs(ft_abs(flg.prec_mod)
 				- ft_ncsigned(num)) - spaces;
