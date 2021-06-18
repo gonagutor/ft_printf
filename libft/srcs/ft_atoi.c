98c2f264f108e8488c25050adc352778afb5fbc7
Gonzalo Aguado Torres <gonagutor@gmail.com>
2021-06-18 18:53:35 +0200

    Updated norminette for atoi

--- libft/srcs/ft_atoi.c
+++ libft/srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: gaguado- <gaguado-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/03 19:08:25 by gaguado-          #+#    #+#             */
-/*   Updated: 2021/05/05 19:09:55 by gaguado-         ###   ########.fr       */
+/*   Updated: 2021/06/18 18:50:36 by gaguado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,7 @@ int	ft_atoi(const char *str)
 		|| *str == '\f' || *str == ' ')
 		str++;
 	if (*str == '+' || *str == '-')
-	{
-		if (*str++ == '+')
-			neg = 1;
-		else
-			neg = -1;
-	}
+		neg = !(*str++ == '+') * -1;
 	while (*str > 47 && *str < 58)
 	{
 		icheck = neg * ((long)rtn * 10 + (long)*str - 48);
